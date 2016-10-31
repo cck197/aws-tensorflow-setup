25dff8047618255ffeb18d740b9cb1ef121ec2af
CHRISTOPHER KELLY <chris@nourishbalancethrive.com>
2016-10-31 13:59:25 -0700

    chmod

--- setup-aws-tensorflow.bash
+++ setup-aws-tensorflow.bash
@@ -12,28 +12,18 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
 
 # install cuda
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 sudo apt-get update
 sudo apt-get install -y cuda
 
 # get cudnn
-CUDNN_FILE=cudnn-7.0-linux-x64-v4.0-prod.tgz
-wget http://developer.download.nvidia.com/compute/redist/cudnn/v4/${CUDNN_FILE}
-tar xvzf ${CUDNN_FILE}
-rm ${CUDNN_FILE}
-sudo cp cuda/include/cudnn.h /usr/local/cuda/include # move library files to /usr/local/cuda
-sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-rm -rf cuda
-
-# set the appropriate library path
-echo 'export CUDA_HOME=/usr/local/cuda
-export CUDA_ROOT=/usr/local/cuda
-export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
-' >> ~/.bashrc
+wget https://s3.amazonaws.com/cck197-cadl/libcudnn5_5.1.5-1%2Bcuda8.0_amd64.deb
+wget https://s3.amazonaws.com/cck197-cadl/libcudnn5-dev_5.1.5-1%2Bcuda8.0_amd64.deb
+sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
+sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
 
 # install anaconda
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
