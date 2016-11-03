0e4f8868e3ee3083b7327bc1104941062ab0d931
CHRISTOPHER KELLY <chris@nourishbalancethrive.com>
2016-11-03 09:08:26 -0700

    works perfect

--- setup-aws-tensorflow.bash
+++ setup-aws-tensorflow.bash
@@ -4,15 +4,17 @@
 set -e
 ############################################
 # install into /mnt/bin
-sudo mkdir -p /mnt/bin
-sudo chown ubuntu:ubuntu /mnt/bin
+sudo mkdir -p /mnt/ubuntu
+sudo chown ubuntu:ubuntu /mnt/ubuntu
+cd /mnt/ubuntu
 
 # install the required packages
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
 
+sudo apt-get install imagemagick
+
 # install cuda
-#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
@@ -20,21 +22,21 @@ sudo apt-get update
 sudo apt-get install -y cuda
 
 # get cudnn
-wget https://s3.amazonaws.com/cck197-cadl/libcudnn5_5.1.5-1%2Bcuda8.0_amd64.deb
 wget https://s3.amazonaws.com/cck197-cadl/libcudnn5-dev_5.1.5-1%2Bcuda8.0_amd64.deb
-sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
-sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
+wget https://s3.amazonaws.com/cck197-cadl/libcudnn5_5.1.5-1%2Bcuda8.0_amd64.deb
+sudo dpkg -i libcudnn*
 
-# install anaconda
-wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
-rm Anaconda3-4.0.0-Linux-x86_64.sh
-echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
+git clone git@github.com:cck197/CADL.git
+cd CADL
+virtualenv -p python3 venv
+. venv/bin/activate
+TMPDIR=.. pip install -r requirements.txt
 
 # install tensorflow
-export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
+# export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp35-cp35m-linux_x86_64.whl
 
-/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
+
+#/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
 
 # install monitoring programs
 sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
